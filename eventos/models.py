2483f3337470bf41260e257719dda503b1b76671
VesnaPivac <33427475+VesnaPivac@users.noreply.github.com>
2021-12-02 12:09:48 -0700

    Delete eventos directory

--- eventos/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User,Group, AbstractUser
-from django.db.models.base import Model
-
-# Create your models here.
-class Usuario(models.Model):
-    nombre = models.CharField(max_length=100,null=True)
